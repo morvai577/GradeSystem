0b612c1c109f86c9ab027020b85b5b9ee58364fe
Vaibhav Mor <Vaibhav Mor>
2016-07-23 22:02:21 +1200

    Initial Creation of Project

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+
+
+        }
+
+        List<float> grades;
+    }
+
+
+}
